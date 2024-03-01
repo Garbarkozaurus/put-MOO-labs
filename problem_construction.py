31b2f931d8a13f381645429b69d15dc8c2526e66
Garbarkozaurus <marcin.golski@student.put.poznan.pl>
2024-03-01 12:23:37 +0100

    Construction of solvers for weighted sum approach
    Also refactored creating normalization and nonnegativity constraints out
    of older functions. `git add -p` was unable to split the changes into
    appropriate hunks, I really didn't want to make this commit so big and
    disconnected, I swear

--- problem_construction.py
+++ problem_construction.py
@@ -6,6 +6,30 @@ import data_loading
 import utils
 
 
+def nonnegativity_constraints_matrices(n: int) -> tuple[cvxopt.matrix]:
+    """Returns a pair of matrices:
+    - coefficients of non-negativity constraints
+    (a diagonal n x n matrix with -1 as diagonal value to model >=)
+    - bounds of non-negativity constraints (n repeats of 0.0)
+    """
+    # every row in the argument of cvxopt.matrix is a column
+    # representing the coefficients for a variable in all constraints
+    nonnegativity_constraints_coefs = cvxopt.matrix(np.diagflat([-1.0] * n))
+    nonnegativity_constraints_bounds = cvxopt.matrix([0.0] * n)
+    return nonnegativity_constraints_coefs, nonnegativity_constraints_bounds
+
+
+def normalization_constraint_matrices(n: int) -> tuple[cvxopt.matrix]:
+    """Returns a pair of matrices:
+    - coefficients of the normalization constraint
+    (a 1xn matrix of 1.0)
+    - bounds of non-negativity constraints (a single 1.0)
+    """
+    normalization_constraint_coefs = cvxopt.matrix([[1.0] for _ in range(n)])
+    normalization_constraint_bound = cvxopt.matrix([1.0])
+    return normalization_constraint_coefs, normalization_constraint_bound
+
+
 def maximize_return_solver(companies: list[Company]):
     """IMPORTANT: this function assumes that the values of expected return
     have already been calculated and stored in the expected_return
@@ -20,17 +44,18 @@ def maximize_return_solver(companies: list[Company]):
     # is redundant, but it serves as a neat example of defining a cvxopt solver
     expected_returns = [-1*company.expected_return for company in companies]
     exp_ret_matrix = cvxopt.matrix(expected_returns)
-    normalization_constraint_coefs = cvxopt.matrix([[1.0] for _ in companies])
-    # -1, because that's how >= is expressed
-    nonnegativity_constraints_coefs = cvxopt.matrix(np.diagflat([-1.0 for _ in companies]))
-    # every row in the argument is a column in the constraint matrix
-    # representing the coefficients for variables in all constraints
-    normalization_constraint_bound = cvxopt.matrix([1.0])
-    nonnegativity_constraints_bounds = cvxopt.matrix([0.0] * len(companies))
+
+    num_companies = len(companies)
+    # nonnegativity_constraints_bounds = cvxopt.matrix([0.0] * len(companies))
+    nonnegativity_constraints_coefs, nonnegativity_constraints_bounds = \
+        nonnegativity_constraints_matrices(num_companies)
+    normalization_constraint_coefs, normalization_constraint_bound = \
+        normalization_constraint_matrices(num_companies)
     return cvxopt.solvers.lp(exp_ret_matrix, nonnegativity_constraints_coefs,
                              nonnegativity_constraints_bounds,
                              normalization_constraint_coefs,
-                             normalization_constraint_bound)
+                             normalization_constraint_bound,
+                             options={'show_progress': False})
 
 
 def minimize_risk_solver(companies: list[Company]):
@@ -38,38 +63,86 @@ def minimize_risk_solver(companies: list[Company]):
     risk_matrix = cvxopt.matrix(covariance_matrix)
     # coefficients for the linear component of the optimized function
     c = cvxopt.matrix([0.0 for _ in companies])
-    normalization_constraint_coefs = cvxopt.matrix([[1.0] for _ in companies])
-    # -1, because that's how >= is expressed
-    nonnegativity_constraints_coefs = cvxopt.matrix(np.diagflat([-1.0 for _ in companies]))
-    # every row in the argument is a column in the constraint matrix
-    # representing the coefficients for variables in all constraints
-    normalization_constraint_bound = cvxopt.matrix([1.0])
-    nonnegativity_constraints_bounds = cvxopt.matrix([0.0] * len(companies))
+    num_companies = len(companies)
+    # nonnegativity_constraints_bounds = cvxopt.matrix([0.0] * len(companies))
+    nonnegativity_constraints_coefs, nonnegativity_constraints_bounds = \
+        nonnegativity_constraints_matrices(num_companies)
+    normalization_constraint_coefs, normalization_constraint_bound = \
+        normalization_constraint_matrices(num_companies)
     return cvxopt.solvers.qp(risk_matrix, c, nonnegativity_constraints_coefs,
                              nonnegativity_constraints_bounds,
                              normalization_constraint_coefs,
-                             normalization_constraint_bound)
+                             normalization_constraint_bound,
+                             options={'show_progress': False})
+
+
+def exp_ret_risk_spreads(
+        companies: list[Company],
+        history_len: int | None = None) -> tuple[float, float]:
+    """Returns tuple: (expected_return_spread, risk_spread)"""
+    solver_max_ret = maximize_return_solver(companies)
+    max_ret_ret = -1*solver_max_ret["primal objective"]
+    max_ret_weights = np.array(solver_max_ret["x"]).flatten()
+    max_ret_risk = utils.portfolio_risk(companies, max_ret_weights, history_len)
+    solver_min_risk = minimize_risk_solver(companies)
+    min_risk_risk = solver_min_risk["primal objective"]
+    min_risk_weights = np.array(solver_min_risk["x"]).flatten()
+    min_risk_ret = utils.portfolio_expected_return(companies, min_risk_weights)
+    ret_spread = max_ret_ret - min_risk_ret
+    risk_spread = max_ret_risk - min_risk_risk
+    return ret_spread, risk_spread
 
 
 def weighted_sum_solver(
         companies: list[Company],
-        profit_weight: float,
+        ret_weight: float,
         risk_weight: float,
+        ret_spread: float,
+        risk_spread: float,
         history_len: int | None = None) -> cvxopt.solvers.qp:
     """IMPORTANT: this function assumes that the values of expected return
     have already been calculated and stored in the expected_return
     field of appropriate companies"""
-    # TODO
+    # Objective function
     # remember to normalize by dividing over the spread!
-    pass
+    expected_returns = [-1*company.expected_return for company in companies]
+    exp_ret_matrix = cvxopt.matrix(expected_returns)
+    exp_ret_matrix /= ret_spread
+    exp_ret_matrix *= ret_weight
+    covariance_matrix = utils.covariance_matrix_from_companies(companies, history_len)
+    risk_matrix = cvxopt.matrix(covariance_matrix)
+    risk_matrix /= risk_spread
+    risk_matrix *= risk_weight
+
+    # Constraints
+    num_companies = len(companies)
+    nonnegativity_constraints_coefs, nonnegativity_constraints_bounds = \
+        nonnegativity_constraints_matrices(num_companies)
+    normalization_constraint_coefs, normalization_constraint_bound = \
+        normalization_constraint_matrices(num_companies)
+    return cvxopt.solvers.qp(
+        risk_matrix, exp_ret_matrix,
+        nonnegativity_constraints_coefs, nonnegativity_constraints_bounds,
+        normalization_constraint_coefs, normalization_constraint_bound,
+        options={'show_progress': False})
 
 
 if __name__ == "__main__":
+    EXPECTED_RETURN_WEIGHT = 1.0
+    RISK_WEIGHT = 1.0 - EXPECTED_RETURN_WEIGHT
     companies = data_loading.load_all_companies_from_dir("./data/Bundle1")
     for i, c in enumerate(companies):
         c.expected_return = (i+1)/20
     s = maximize_return_solver(companies)
     # s = minimize_risk_solver(companies)
-    print(s.keys())
-    print(s["primal objective"])
-    print(s["x"])
+    # print(s.keys())
+    # print(s["x"])
+    # print(s["primal objective"])
+
+    exp_ret_spread, risk_spread = exp_ret_risk_spreads(companies)
+    w_s = weighted_sum_solver(
+        companies, EXPECTED_RETURN_WEIGHT, RISK_WEIGHT,
+        exp_ret_spread, risk_spread)
+    weights = w_s["x"]
+    risk = utils.portfolio_risk(companies, weights)
+    exp_ret = utils.portfolio_expected_return(companies, weights)
