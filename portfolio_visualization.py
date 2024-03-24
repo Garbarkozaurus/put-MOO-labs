b22bb9bf7a51fbd59cfe3cdc113b9d5d701074b9
Garbarkozaurus <marcin.golski@student.put.poznan.pl>
2024-03-24 12:33:39 +0100

    Take ret from csv in portfolio visualization

--- portfolio_visualization.py
+++ portfolio_visualization.py
@@ -2,14 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from typing import Iterable
 
-
 from company import Company
 import data_loading
-import problem_construction
 import utils
 import return_estimation
 
@@ -55,9 +54,48 @@ def vis_portfolio_linear_regression_subplots(
     plt.show()
 
 
+def vis_portfolio_from_csv_subplots(
+        companies: list[Company], prediction_time: int,
+        weights: Iterable[float],
+        csv_directory: str,
+        plot_timeline_start: int = 0,
+        num_rows: int = 4,
+        export_pdf: bool = False,
+        pdf_title: str = "portfolio.pdf") -> None:
+    if csv_directory[-1] != '/':
+        csv_directory += "/"
+    num_columns = int(np.ceil(len(companies)/num_rows))
+    fig, ax = plt.subplots(nrows=num_rows, ncols=num_columns)
+    for i, company in enumerate(companies):
+        row = i // num_columns
+        column = i % num_columns
+        csv_path = f"{csv_directory}{company.name}.csv"
+        all_predictions = pd.read_csv(csv_path, header=None)
+        prediction_value = return_estimation.expected_return_from_csv(csv_path, prediction_time)
+        ax[row, column].plot(company.prices)
+        ax[row, column].set_xlim([plot_timeline_start, prediction_time])
+        ax[row, column].plot(all_predictions[0], all_predictions[1])
+        ax[row, column].set_title(
+            f"{company.name}: " +
+            f"{np.round((prediction_value/company.prices[-1]-1)*100, 2)}% " +
+            f"{np.round(weights[i], 2)} | "
+            f"{np.round((prediction_value/company.prices[-1]-1) * weights[i], 2)}")
+        ax[row, column].tick_params(
+            axis='x', which='both', bottom=False, top=False, labelbottom=False)
+    # fig.tight_layout()
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
+    plt.gcf().set_size_inches(25 * 0.7, 18 * 0.6)
+    if export_pdf:
+        plt.savefig(pdf_title, format="pdf")
+    plt.show()
+
+
 if __name__ == "__main__":
     companies = data_loading.load_all_companies_from_dir("./data/Bundle1/")
     weights_portfolio1 = [8.644665649845455e-07, 3.2298170638724315e-08, 1.3824240077869665e-08, 2.1611643635980332e-08, 1.8732855958022932e-06, 1.8413226578557183e-08, 1.8843236108265335e-09, 0.0004081023422847454, 0.024427478059861403, 0.3660731075758385, 2.987919062764484e-08, 0.3668022084828672, 4.23305172698446e-08, 0.061618815871245035, 0.18066607274170104, 3.706977748432357e-08, 6.306775209173726e-08, 1.0251652094173745e-08, 5.559817292282156e-07, 6.505618178746683e-07]
     vis_portfolio_linear_regression_subplots(companies, 200, weights_portfolio1)
-    for company in companies:
-        company.expected_return = return_estimation.predict_expected_return_linear_regression(company, 200)[0]
+    # for company in companies:
+    #     company.expected_return = return_estimation.predict_expected_return_linear_regression(company, 200)[0]
+    ordered_weights = utils.weights_in_valid_order(companies, weights_portfolio1)
+    for company, weight in zip(utils.COMPANY_ORDER, ordered_weights):
+        print(f"{company:19} & {round(weight, 5):0f}")
